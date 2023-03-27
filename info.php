54193dd2efd7559fbd6a747f829aa6fab89bdaf3
daniejl04 <danieljimenez@MacBook-Air-de-Daniel.local>
2023-03-27 07:28:39 -0500

    inicial

---
+++ info.php
@@ -0,0 +1,9 @@
+<?php include("./template/header.php");?>
+
+     <div class="form-group">
+       <label for=""></label>
+       <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="">
+       <small id="emailHelpId" class="form-text text-muted">Help text</small>
+     </div>
+            
+<?php include("template/footer.php")?>
---
+++ template/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../img/tyara-rpve-dorado.png">
+    <title>Tyara</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bad+Script&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="../css/style.css">  
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid ">
+  <a class="navbar-brand p-5" href="../index.php">
+      <img src="../img/tyara-logo.svg" alt="Tyara" width="35%" height="auto">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <ul class="navbar-nav me-auto mb-4 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+        </li>
+        <!-- <li class="nav-item">
+          <a class="nav-link" href="../info.php">Info</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../nosotros.php">Nosotros</a>
+        </li> -->
+      </ul>
+      <!-- <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form> -->
+    </div>
+  </div>
+</nav>
+    <div class="container">
+        <div class="row">

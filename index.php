d52d1be2e6676d1ecf9ebcb1a70c69a4fa18151b
daniejl04 <danielestebanjimenezlopez@gmail.com>
2023-03-29 19:28:16 -0500

    ay

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <?php include("./template/header.php");?>
 
-            <div class="p-5 bg-light">
+            <div class="p-3 bg-light">
                 <div class="container ">
                         <h1 class="display-3 text-center">Portafolio Especial</h1>
                         <hr class="my-3">
                     <div class="row">
                         <div class="grid text-center"> 
-                        <iframe src="archivos/accesorios.pdf" height="800px" width="100%"></iframe>
+                            <object class="pdfview" type="application/pdf" data="archivos/accesorios.pdf" height="800px" width="100%" ></object>
+                        <!-- <iframe src="archivos/accesorios.pdf" height="800px" width="100%">
+                        </iframe> -->
                             <!-- <embed src="archivos/accesorios.pdf" type="application/pdf" width="100%" height="800px"  /> -->
                         </div>   
                         <a class="btn btn-primary btn-lg aling-center" href="archivos/accesorios.pdf" role="button">Descargar</a>
                     </div>
                 </div>
             </div>
+            
             
 <?php include("template/footer.php")?>

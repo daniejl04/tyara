e7ee58ea91a93e2be9c00218e0262b40a78c5050
daniejl04 <danielestebanjimenezlopez@gmail.com>
2023-04-26 12:33:38 -0500

    intento git 2

---
+++ content/ult-productos.php
@@ -0,0 +1,31 @@
+
+
+<div class="row row-cols-1 row-cols-md-3 g-4 my-3" id="card-prd">
+  <div class="col">
+    <div class="card h-100">
+      <img src="../img/31BA57E7E151259.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Arete </h5>
+        <p class="card-text">precio</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="../img/31BA57E7E15125B.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Arete 2</h5>
+        <p class="card-text">precio</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="../img/31BA57E7E15125F.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Arete 3</h5>
+        <p class="card-text">precio</p>
+      </div>
+    </div>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -1,20 +1,18 @@
 <?php include("./template/header.php");?>
 
-            <div class="p-3 bg-light">
-                <div class="container ">
-                        <h1 class="display-3 text-center">Portafolio Especial</h1>
-                        <hr class="my-3">
-                    <div class="row">
-                        <div class="grid text-center"> 
-                            <object class="pdfview" type="application/pdf" data="archivos/accesorios.pdf" height="800px" width="100%" ></object>
-                        <!-- <iframe src="archivos/accesorios.pdf" height="800px" width="100%">
-                        </iframe> -->
-                            <!-- <embed src="archivos/accesorios.pdf" type="application/pdf" width="100%" height="800px"  /> -->
-                        </div>   
-                        <a class="btn btn-primary btn-lg aling-center" href="archivos/accesorios.pdf" role="button">Descargar</a>
-                    </div>
-                </div>
-            </div>
-            
-            
+ <?php include("./content/ult-productos.php");?> 
+
+<div class="p-3 bg-light">
+    <div class="container ">
+        <h1 class="display-3 text-center">Portafolio Especial</h1>
+        <hr class="my-3">
+        <div class="row">
+                <a class="btn btn-primary btn-lg aling-center" href="archivos/accesorios.pdf" role="button">Descargar</a>
+                <br>
+            <div class="grid text-center"> 
+                <object class="pdfview" type="application/pdf" data="archivos/accesorios.pdf" height="800px" width="100%" ></object>
+            </div>   
+        </div>
+    </div>
+</div>    
 <?php include("template/footer.php")?>
